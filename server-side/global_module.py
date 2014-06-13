112239d44ed318c8e49c0b6f12148d3c17ea338b
ankurgarg101 <ankurgarg101@gmail.com>
2014-06-13 18:19:59 +0530

    Adds global variables file

---
+++ server-side/global_module.py
@@ -0,0 +1,6 @@
+# the module contains all the global variables that can be different files at the same time 
+isConnected = False
+wifi_namespace_reference = 0
+current_session = []
+logged = []
+db_cursor =  None
